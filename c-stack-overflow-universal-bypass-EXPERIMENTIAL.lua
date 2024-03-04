52e82cd7a9accc0e04fc3de9e4ae9d470bc2b5c1
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2024-03-03 16:10:12 -0800

    Update c-stack-overflow-universal-bypass-EXPERIMENTIAL.lua

--- c-stack-overflow-universal-bypass-EXPERIMENTIAL.lua
+++ c-stack-overflow-universal-bypass-EXPERIMENTIAL.lua
@@ -26,13 +26,15 @@ local function insertincache(func, ofunc)
             local __args = {pcall((h or coroutine.wrap)(ofunc), ...)}
             local bigerr = __args[2]
 
-            if cachevalue[2] > 198 and bigerr ~= "cannot resume dead coroutine" and select(2, pcall((h or coroutine.wrap)(func))) ~= "cannot resume dead coroutine" then
+            --[[if cachevalue[2] > 198 and bigerr ~= "cannot resume dead coroutine" and select(2, pcall((h or coroutine.wrap)(func))) ~= "cannot resume dead coroutine" then
                 task.spawn(cachevalue[4])
                 return error("C stack overflow", 2)
             elseif bigerr == "cannot resume dead coroutine" then
                 task.spawn(cachevalue[4])
                 return error("cannot resume dead coroutine", 2)
-            end
+            end]]
+
+            warn(bigerr, select(2, pcall(h(func))))
             task.spawn(cachevalue[4])
 
             if __args[1] then return select(2, unpack(__args)) end
