bb7f873e343558d8b2b34911bbc425966c926f90
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2024-03-12 21:34:45 -0700

    Create debug-traceback-bypass.lua

---
+++ debug-traceback-bypass.lua
@@ -0,0 +1,11 @@
+local exploitsrc = debug.info(1, "s")
+
+local h; h = hookfunction(getrenv().debug.traceback, function(...)
+    local res = h(...)
+
+    if not checkcaller() and typeof(res) == "string" and string.match(res, exploitsrc) then
+        res = string.gsub(res, exploitsrc .. ":%d+\n", "")
+    end
+
+    return res
+end)
