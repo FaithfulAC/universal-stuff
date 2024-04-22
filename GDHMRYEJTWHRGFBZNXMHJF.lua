04f5351940dbc3e58d05ac6de15d66ed78ddd13f
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2024-04-22 14:26:53 -0700

    Update GDHMRYEJTWHRGFBZNXMHJF.lua

--- GDHMRYEJTWHRGFBZNXMHJF.lua
+++ GDHMRYEJTWHRGFBZNXMHJF.lua
@@ -1,3 +1,3 @@
-if true then
-  return
+if clear_teleport_queue or identifyexecutor():find("Kr") or select(2, identifyexecutor()):find("v1") or gethui() == game:GetService("CoreGui") then
+  getrenv().wait(9e9)
 end
