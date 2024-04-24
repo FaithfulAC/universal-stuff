84bb66e1d5faafc0fd21a77c1301c0c9f2079312
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2024-04-24 16:22:52 -0700

    Update true-secure-dex.lua

--- true-secure-dex.lua
+++ true-secure-dex.lua
@@ -1,65 +1,30 @@
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/universal-stuff/main/true-secure-dex.lua"))()
-
-if not getgenv().TSDBypassesOff then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/universal-stuff/main/true-secure-dex-bypasses.lua"))()
-end
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/universal-stuff/main/true-secure-dex-bypasses.lua"))()
 task.wait(.2)
 
-getgenv().Dex = game:GetObjects("rbxassetid://14878398926")[1]
+-- it's broken, fuck! revert to babyhamsta's model i guess :(
+--getgenv().Dex = game:GetObjects("rbxassetid://14878398926")[1]
 
-Dex.Parent = (gethui and gethui() ~= game:GetService("CoreGui") and gethui()) or (pcall(function()
-		game:GetService("CoreGui"):GetFullName()
-	end) and game:GetService("CoreGui").RobloxGui) or game:GetService("Players").LocalPlayer and Instance.new("ScreenGui", game:GetService("Players").LocalPlayer.PlayerGui)
+getgenv().Dex = game:GetObjects("rbxassetid://9352453730")[1]
+Dex.Parent = (gethui and gethui() ~= game:GetService("CoreGui") and gethui()) or game:GetService("CoreGui").RobloxGui
 
--- lol silly forking
-local charset = {}
-for i = 48,  57 do table.insert(charset, string.char(i)) end
-for i = 65,  90 do table.insert(charset, string.char(i)) end
-for i = 97, 122 do table.insert(charset, string.char(i)) end
-function RandomCharacters(length)
-	if length > 0 then
-		return RandomCharacters(length - 1) .. charset[math.random(1, #charset)]
-	else
-		return ""
-	end
-end
-Dex.Name = RandomCharacters(math.random(7,13))
+do
+	local characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	local name = ""
 
-local function Load(Obj, Url)
-	local function GiveOwnGlobals(Func, Script)
-		local Fenv = {}
-		local RealFenv = {script = Script}
-		local FenvMt = {}
-		function FenvMt:__index(b)
-			if RealFenv[b] == nil then
-				return getfenv()[b]
-			else
-				return RealFenv[b]
-			end
-		end
-		function FenvMt:__newindex(b, c)
-			if RealFenv[b] == nil then
-				getfenv()[b] = c
-			else
-				RealFenv[b] = c
-			end
-		end
-		setmetatable(Fenv, FenvMt)
-		setfenv(Func, Fenv)
-		return Func
+	for i = 1, math.random(10, 19) do
+		local randint = math.random(#characters)
+		name = name .. string.sub(characters, randint, randint)
 	end
 
-	local function LoadScripts(Script)
-		if Script.ClassName == "Script" or Script.ClassName == "LocalScript" then
-			task.spawn(GiveOwnGlobals(loadstring(Script.Source, "=" .. Script:GetFullName()), Script))
-		end
-		for _,v in ipairs(Script:GetChildren()) do
-			LoadScripts(v)
-		end
-	end
-
-	LoadScripts(Obj)
+	Dex.Name = name
 end
 
-Load(Dex)
+for i, Script in Dex:GetDescendants() do
+	if Script.ClassName == "Script" or Script.ClassName == "LocalScript" then
+		local func = loadstring(Script.Source, "=" .. Script:GetFullName())
+		getfenv(func).script = Script
+		--setfenv(func, 1)
+		
+		task.spawn(func)
+	end
+end
