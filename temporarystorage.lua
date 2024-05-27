19810c6dfb038b9517ef2ed96358d6bccf0ea9ee
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2024-05-27 08:58:57 -0700

    Update temporarystorage.lua

--- temporarystorage.lua
+++ temporarystorage.lua
@@ -8,32 +8,32 @@ local spoofcache = {}
 local split, format = string.split, string.format
 
 local __newindex; __newindex = hookmetamethod(game, "__newindex", function(...)
-    local self, prop = ...
+	local self, prop = ...
 
-    if not checkcaller() and typeof(self) == "Instance" and typeof(prop) == "string" then
-        prop = split(prop, "\0")[1]
-            
-        for i, v in pairs(spoofcache) do
-            if compareinstances(v.Instance, self) and prop == v.Property and pcall(__newindex, ...) then
-                local message = format("Unable to assign property %s. Property is read only", prop)
+	if not checkcaller() and typeof(self) == "Instance" and typeof(prop) == "string" then
+		prop = split(prop, "\0")[1]
 
-                return error(message, 0)
-            end
-        end
-    end
+		for i, v in pairs(spoofcache) do
+			if compareinstances(v.Instance, self) and prop == v.Property and pcall(__newindex, ...) then
+				local message = format("Unable to assign property %s. Property is read only", prop)
 
-    return __newindex(...)
+				return error(message, 0)
+			end
+		end
+	end
+
+	return __newindex(...)
 end)
 
 local setpropertyreadonly = function(ins, prop, bool)
-    if bool then
-        table.insert(spoofcache, {Instance = cloneref(ins), Property = prop})
-    else
-        for i, v in pairs(spoofcache) do
-            if compareinstances(v.Instance, ins) then
-                table.remove(spoofcache, i)
-                break
-            end
-        end
-    end
+	if bool then
+		table.insert(spoofcache, {Instance = cloneref(ins), Property = prop})
+	else
+		for i, v in pairs(spoofcache) do
+			if compareinstances(v.Instance, ins) then
+				table.remove(spoofcache, i)
+				break
+			end
+		end
+	end
 end
