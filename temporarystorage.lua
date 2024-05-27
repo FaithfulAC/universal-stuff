53754d6f2c75c54aab33f8a2d6cc078f8132c1d5
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2024-05-27 09:25:47 -0700

    Update temporarystorage.lua

--- temporarystorage.lua
+++ temporarystorage.lua
@@ -5,19 +5,31 @@
 -- setpropertyreadonly(<Instance>, <string>, <bool>)
 
 local spoofcache = {}
-local split, format = string.split, string.format
+local preventioncache = {}
+local split, format, gsub, lower, upper = string.split, string.format, string.gsub, string.lower, string.upper
 
 local __newindex; __newindex = hookmetamethod(game, "__newindex", function(...)
 	local self, prop = ...
 
 	if not checkcaller() and typeof(self) == "Instance" and typeof(prop) == "string" then
 		prop = split(prop, "\0")[1]
+		local checksub = false
+			
+		if pcall(function() return self[gsub(prop, "^%u", lower)] end) and self[gsub(prop, "^%u", lower)] == self[prop] then
+			checksub = true
+		end
 
 		for i, v in pairs(spoofcache) do
-			if compareinstances(v.Instance, self) and prop == v.Property then
+			if compareinstances(v.Instance, self) and ((checksub and gsub(prop, "^%l", upper)) or prop) == v.Property then
 				local message = format("Unable to assign property %s. Property is read only", prop)
 
 				return error(message, 0)
+			end
+		end
+
+		for i, v in pairs(preventioncache) do
+			if compareinstances(v.Instance, self) and ((checksub and gsub(prop, "^%l", upper)) or prop) == v.Property then
+				return;
 			end
 		end
 	end
@@ -33,6 +45,19 @@ local setpropertyreadonly = function(ins, prop, bool)
 		for i, v in pairs(spoofcache) do
 			if compareinstances(v.Instance, ins) then
 				table.remove(spoofcache, i)
+				break
+			end
+		end
+	end
+end
+
+local setpropertyunchangeable = function(ins, prop, bool)
+	if bool then
+		table.insert(preventioncache, {Instance = cloneref(ins), Property = prop})
+	else
+		for i, v in pairs(preventioncache) do
+			if compareinstances(v.Instance, ins) then
+				table.remove(prevevntioncache, i)
 				break
 			end
 		end
