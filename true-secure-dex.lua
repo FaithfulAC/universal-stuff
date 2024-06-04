cd2463fabdfd229e1b58a8cad3a8def8f20544f7
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2024-06-04 12:59:13 -0700

    Update true-secure-dex.lua

--- true-secure-dex.lua
+++ true-secure-dex.lua
@@ -4,9 +4,9 @@ end
 
 task.wait(.2)
 
-getgenv().Dex = game:GetObjects("rbxassetid://14878398926")[1]
+--getgenv().Dex = game:GetObjects("rbxassetid://14878398926")[1]
 
---getgenv().Dex = game:GetObjects("rbxassetid://9352453730")[1]
+getgenv().Dex = game:GetObjects("rbxassetid://9352453730")[1]
 Dex.Parent = (gethui and gethui() ~= game:GetService("CoreGui") and gethui()) or game:GetService("CoreGui").RobloxGui
 
 do
