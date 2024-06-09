83a4aed33319ca27a6b2e7b35538b87891c752a0
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2024-06-09 16:43:47 -0700

    Update lua-getproperties.lua

--- lua-getproperties.lua
+++ lua-getproperties.lua
@@ -3,9 +3,9 @@ local HttpService = cloneref(game:GetService("HttpService"))
 local url = "https://raw.githubusercontent.com/MaximumADHD/Roblox-Client-Tracker/roblox/API-Dump.json"
 
 local function fetchData(url)
-    local response = game:HttpGet(url)
-    local data = HttpService:JSONDecode(response)
-    return data
+	local response = game:HttpGet(url)
+	local data = HttpService:JSONDecode(response)
+	return data
 end
 
 local apiData = fetchData(url)
@@ -17,127 +17,127 @@ local function sortAlphabetic(t, property)
 end
 
 getgenv().getproperties = function(class)
-    local properties = {}
+	local properties = {}
 
-    if typeof(class) == "Instance" then
-        class = class.ClassName
-    elseif typeof(class) ~= "string" then
-        return error("bad argument #1 (string or Instance expected)")
-    end
-    
-    for i, otherclass in ipairs(apiData.Classes) do
-        if otherclass.Name == "Instance" then
-            for _, member in ipairs(otherclass.Members) do
-                if member.MemberType == "Property" then
-                    table.insert(properties, member)
-                end
-            end
-        elseif class == otherclass.Name then
-            for _, member in ipairs(otherclass.Members) do
-                if member.MemberType == "Property" then
-                    table.insert(properties, member)
-                end
-            end
-            
-            break
-        end
-    end
+	if typeof(class) == "Instance" then
+		class = class.ClassName
+	elseif typeof(class) ~= "string" then
+		return error("bad argument #1 (string or Instance expected)")
+	end
 
-    sortAlphabetic(properties, "Name")
-    return properties
+	for i, otherclass in ipairs(apiData.Classes) do
+		if otherclass.Name == "Instance" then
+			for _, member in ipairs(otherclass.Members) do
+				if member.MemberType == "Property" then
+					table.insert(properties, member)
+				end
+			end
+		elseif class == otherclass.Name then
+			for _, member in ipairs(otherclass.Members) do
+				if member.MemberType == "Property" then
+					table.insert(properties, member)
+				end
+			end
+
+			break
+		end
+	end
+
+	sortAlphabetic(properties, "Name")
+	return properties
 end
 
 getgenv().getfunctions = function(class)
-    local functions = {}
+	local functions = {}
 
-    if typeof(class) == "Instance" then
-        class = class.ClassName
-    elseif typeof(class) ~= "string" then
-        return error("bad argument #1 (string or Instance expected)")
-    end
-    
-    for i, otherclass in ipairs(apiData.Classes) do
-        if otherclass.Name == "Instance" then
-            for _, member in ipairs(otherclass.Members) do
-                if member.MemberType == "Function" then
-                    table.insert(functions, member)
-                end
-            end
-        elseif class == otherclass.Name then
-            for _, member in ipairs(otherclass.Members) do
-                if member.MemberType == "Function" then
-                    table.insert(functions, member)
-                end
-            end
-            
-            break
-        end
-    end
+	if typeof(class) == "Instance" then
+		class = class.ClassName
+	elseif typeof(class) ~= "string" then
+		return error("bad argument #1 (string or Instance expected)")
+	end
 
-    sortAlphabetic(functions, "Name")
-    return functions
+	for i, otherclass in ipairs(apiData.Classes) do
+		if otherclass.Name == "Instance" then
+			for _, member in ipairs(otherclass.Members) do
+				if member.MemberType == "Function" then
+					table.insert(functions, member)
+				end
+			end
+		elseif class == otherclass.Name then
+			for _, member in ipairs(otherclass.Members) do
+				if member.MemberType == "Function" then
+					table.insert(functions, member)
+				end
+			end
+
+			break
+		end
+	end
+
+	sortAlphabetic(functions, "Name")
+	return functions
 end
 
 getgenv().getevents = function(class)
-    local events = {}
+	local events = {}
 
-    if typeof(class) == "Instance" then
-        class = class.ClassName
-    elseif typeof(class) ~= "string" then
-        return error("bad argument #1 (string or Instance expected)")
-    end
-    
-    for i, otherclass in ipairs(apiData.Classes) do
-        if otherclass.Name == "Instance" then
-            for _, member in ipairs(otherclass.Members) do
-                if member.MemberType == "Event" then
-                    table.insert(events, member)
-                end
-            end
-        elseif class == otherclass.Name then
-            for _, member in ipairs(otherclass.Members) do
-                if member.MemberType == "Event" then
-                    table.insert(events, member)
-                end
-            end
-            
-            break
-        end
-    end
+	if typeof(class) == "Instance" then
+		class = class.ClassName
+	elseif typeof(class) ~= "string" then
+		return error("bad argument #1 (string or Instance expected)")
+	end
 
-    sortAlphabetic(events, "Name")
-    return events
+	for i, otherclass in ipairs(apiData.Classes) do
+		if otherclass.Name == "Instance" then
+			for _, member in ipairs(otherclass.Members) do
+				if member.MemberType == "Event" then
+					table.insert(events, member)
+				end
+			end
+		elseif class == otherclass.Name then
+			for _, member in ipairs(otherclass.Members) do
+				if member.MemberType == "Event" then
+					table.insert(events, member)
+				end
+			end
+
+			break
+		end
+	end
+
+	sortAlphabetic(events, "Name")
+	return events
 end
 
 getgenv().getcallbacks = function(class)
-    local callbacks = {}
+	local callbacks = {}
 
-    if typeof(class) == "Instance" then
-        class = class.ClassName
-    elseif typeof(class) ~= "string" then
-        return error("bad argument #1 (string or Instance expected)")
-    end
-    
-    for i, otherclass in ipairs(apiData.Classes) do
-        if otherclass.Name == "Instance" then
-            for _, member in ipairs(otherclass.Members) do
-                if member.MemberType == "Callback" then
-                    table.insert(callbacks, member)
-                end
-            end
-        elseif class == otherclass.Name then
-            for _, member in ipairs(otherclass.Members) do
-                if member.MemberType == "Callback" then
-                    table.insert(callbacks, member)
-                end
-            end
-            
-            break
-        end
-    end
+	if typeof(class) == "Instance" then
+		class = class.ClassName
+	elseif typeof(class) ~= "string" then
+		return error("bad argument #1 (string or Instance expected)")
+	end
 
-    sortAlphabetic(callbacks, "Name")
-    return callbacks
+	for i, otherclass in ipairs(apiData.Classes) do
+		if otherclass.Name == "Instance" then
+			for _, member in ipairs(otherclass.Members) do
+				if member.MemberType == "Callback" then
+					table.insert(callbacks, member)
+				end
+			end
+		elseif class == otherclass.Name then
+			for _, member in ipairs(otherclass.Members) do
+				if member.MemberType == "Callback" then
+					table.insert(callbacks, member)
+				end
+			end
+
+			break
+		end
+	end
+
+	sortAlphabetic(callbacks, "Name")
+	return callbacks
 end
 
 return apiData
