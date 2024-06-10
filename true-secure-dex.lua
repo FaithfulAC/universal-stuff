06bb9cd46559f80072b2ebcdee7faa9547932c8b
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2024-06-10 14:12:59 -0700

    Update true-secure-dex.lua

--- true-secure-dex.lua
+++ true-secure-dex.lua
@@ -1,13 +1,34 @@
-if getrenv and hookfunction and hookmetamethod and not identifyexecutor():lower():find("solara") then
-	task.spawn(loadstring(game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/universal-stuff/main/true-secure-dex-bypasses.lua")))
+-- hey
+local isfolder, makefolder, isfile, writefile, readfile =
+	isfolder, makefolder, isfile, writefile, readfile;
+
+local getgenv, gethui, getrenv, hookmetamethod, hookfunction, identifyexecutor =
+	getgenv, gethui, getrenv, hookmetamethod, hookfunction, identifyexecutor;
+
+local foldername = "TSDex"
+local path = foldername .. "/lua-getproperties.lua"
+
+if not isfolder(foldername) then
+	makefolder(foldername)
 end
 
-task.wait(.2)
+-- variables to send to scripts
+local Api, gets;
 
--- hey
+-- create lua getproperties file to use from for dex functions next time
+if not isfile(path) then
+	writefile(path, game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/universal-stuff/main/lua-getproperties.lua"))
+end
+
+Api, gets = unpack(loadstring(readfile(path))())
+
 getgenv().Dex = game:GetObjects("rbxassetid://14878398926")[1]
-
 Dex.Parent = (gethui and gethui() ~= game:GetService("CoreGui") and gethui()) or game:GetService("CoreGui").RobloxGui
+
+-- prevent solara from making the damn script error
+if getrenv and hookmetamethod and hookfunction and not identifyexecutor():lower():find("solara") then
+	task.spawn(loadstring(game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/universal-stuff/main/true-secure-dex-bypasses.lua")))
+end
 
 do
 	local characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -18,8 +39,10 @@ do
 		name = name .. string.sub(characters, randint, randint)
 	end
 
+	-- hide dex from plain sight
 	Dex.Name = name
 end
+
 local orgfenv = getfenv()
 
 local scriptlist = {
@@ -30,20 +53,23 @@ local scriptlist = {
 }
 
 local list = {
-	[1] = "Selection.lua",
-	[2] = "Explorer.lua",
-	[3] = "Properties.lua",
-	[4] = "Editor.lua"
+	[1] = "/Selection.lua",
+	[2] = "/Explorer.lua",
+	[3] = "/Properties.lua",
+	[4] = "/Editor.lua"
 }
 
-function GetScript(int)
-	return scriptlist[int]
-end
-
 for i, Script in pairs(scriptlist) do
-	local data = game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/TSD-script-storage/main/" .. list[i])
+	-- set the scripts found in github to files in TSDex folder, so the script runs faster next time
+	local data = readfile(foldername .. list[i])
+
+	if not data then
+		data = game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/TSD-script-storage/main" .. list[i])
+		writefile(foldername .. list[i], data)
+	end
+
 	local func = loadstring(data, "=" .. Script:GetFullName())
-	setfenv(func, getfenv(1)) -- just in case
-	
-	task.spawn(func, GetScript)
+	setfenv(func, orgfenv) -- just in case
+
+	task.spawn(func, Script, Api, gets)
 end
