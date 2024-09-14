6048df30868b0b65d7496427ccf56f923332121f
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2024-09-13 21:35:30 -0700

    Update c-stack-overflow-universal-bypass.lua
    made customizable

--- c-stack-overflow-universal-bypass.lua
+++ c-stack-overflow-universal-bypass.lua
@@ -1,28 +1,70 @@
 --[[
-	i am planning to make this script a customizable script soon enough; this script will take in arguments such as:
+	btw i never actually tested if the customization options work i literally coded this in like 10 or 15 minutes so plz notify me if it no work
+	how to customize the script:
 
-	the stack limit in which to replace the wrapcount (default is 195)
-	a list of functions to exclude from the hook (to avoid detections) or alternatively a list of specific functions that are the only ones to be hooked
-	a boolean value for the above option of excluding/including functions
-	an option to include only c functions or to also include lua functions (however lua functions could perhaps act as a wrapper for c functions watch out for that)
-	2 custom errors that you can use to replace "C stack overflow" and "cannot resume dead coroutine"
+	local args = { -- args should be a table
+		StackThreshold = 195,
+		StackThresholdMax = 198,
+		error1 = "C stack overflow",
+		error2 = "cannot resume dead coroutine",
+		ExcludedFunctions = {},
+		IncludedFunctions = {}, -- if there are any functions in this table then ExcludedFunctions will simply be disregarded
+		IncludeLuaFunctions = true -- if there are any functions in IncludedFunctions then this will automatically be false
+	}
 
-	why? because nothing is detectable if you just bruteforce it enough
+	-- send in the args!
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/universal-stuff/main/c-stack-overflow-universal-bypass.lua"))(args)
 ]]
 
 --[[
 	thanks to these people for helping with detections:
 
 	unlimited (@unlimited_objects)
-	daily (@daily3014)
-	xnx (@xoifail)
-	kar (@kaxr)
-	ludi (@ludi.) -- especially ludi :)
+	daily (@daily3014) -- fixing unpack() logic (and indirectly detections)
+	xnx (@xoifail) -- detections
+	kar (@kaxr) -- detections
+	ludi (@ludi.) -- especially ludi for detections :)
 ]]
 
-local pack, unpack = table.pack, unpack
+local args = (...) or { -- args should be a table
+	StackThreshold = 195,
+	StackThresholdMax = 198,
+	error1 = "C stack overflow",
+	error2 = "cannot resume dead coroutine",
+	ExcludedFunctions = {},
+	IncludedFunctions = {}, -- if there are any functions in this table then ExcludedFunctions will simply be disregarded
+	IncludeLuaFunctions = true -- if there are any functions in IncludedFunctions then this will automatically be false
+}
+
+local stackThreshold = args.StackThreshold or 195
+local stackThresholdMax = args.StackThresholdMax or 198
+local firstError = args.error1 or "C stack overflow"
+local secondError = args.error2 or "cannot resume dead coroutine"
+local excludedFunctions = args.ExcludedFunctions or {}
+local includedFunctions = args.IncludedFunctions or {}
+local includeLuaFunctions = args.includeLuaFunctions or true
+
+local luaCacheFunctions = setmetatable({}, {__mode = "v"}) -- while a weaktable is not completely reliable it helps mitigate most of the cor.wrap detections
+
+if #includedFunctions > 0 then
+	includeLuaFunctions = false
+	table.clear(excludedFunctions)
+end
+
+local pack, unpack, info, find = table.pack, unpack, debug.info, table.find;
 local Cache = {}
 local WrapHook;
+
+local function CheckValidity(func)
+	if (not includeLuaFunctions) and (info(func, "s") ~= "[C]" or table.find(luaCacheFunctions, func)) then
+		return false
+	elseif #includedFunctions > 0 and not find(includedFunctions, func) then
+		return false
+	elseif table.find(excludedFunctions, func) then
+		return false
+	end
+	return true
+end
 
 local function IsInCache(func)
 	for i, tbl in Cache do
@@ -40,18 +82,17 @@ local function InsertInCache(func, wrapped)
 		WrapCount = 1,
 		Original = func,
 		ReplacementFunc = function(...)
-			-- thanks almighty Daily3014 for the unpack correction
 			local args = pack(pcall(WrapHook(func), ...))
 			
 			if not args[1] then
 				local err = args[2]
 				
-				if err ~= "cannot resume dead coroutine" and New.WrapCount > 198 then
+				if err ~= "cannot resume dead coroutine" and New.WrapCount > stackThresholdMax then
 					task.spawn(New.Gc)
-					return error("C stack overflow", 0)
+					return error(firstError, 0)
 				elseif err == "cannot resume dead coroutine" or select(2, pcall(WrapHook(wrapped))) == "cannot resume dead coroutine" then
 					task.spawn(New.Gc)
-					return error("cannot resume dead coroutine", 0)
+					return error(secondError, 0)
 				end
 				
 				task.spawn(New.Gc)
@@ -77,15 +118,23 @@ WrapHook = hookfunction(getrenv().coroutine.wrap, function(...)
 		local CacheTbl = IsInCache(Target)
 		
 		if CacheTbl then
+			local Validity = CheckValidity(Target)
+			if not Validity then
+				if table.find(luaCacheFunctions, Target) then
+					local res = h(...)
+					luaCacheFunctions[table.find(luaCacheFunctions, Target)] = res
+					return res;
+				end
+			end
 			CacheTbl.WrapCount += 1
 			
-			if CacheTbl.WrapCount == 195 then
+			if CacheTbl.WrapCount == stackThreshold then
 				local NewFunc = WrapHook(CacheTbl.ReplacementFunc)
 				CacheTbl.Original, CacheTbl.ReplacementFunc = NewFunc, NewFunc
 				CacheTbl.Wrapped = WrapHook(CacheTbl.Wrapped)
 				
 				return NewFunc
-			elseif CacheTbl.WrapCount < 195 or CacheTbl.WrapCount > 198 then
+			elseif CacheTbl.WrapCount < stackThreshold or CacheTbl.WrapCount > stackThresholdMax then
 				local NewFunc = WrapHook(CacheTbl.Wrapped)
 				CacheTbl.Wrapped = NewFunc
 				
