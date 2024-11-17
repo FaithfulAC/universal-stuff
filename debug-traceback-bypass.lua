03aa623ecb410bbf4b2cb0b67070401253563ef3
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2024-11-17 10:10:48 -0800

    Update debug-traceback-bypass.lua

--- debug-traceback-bypass.lua
+++ debug-traceback-bypass.lua
@@ -1,16 +1,16 @@
 local exploitsrc = debug.info(1, "s") -- if your exploit src changes every execution you're fucked
 
-if exploitsrc == "" then
-    warn("You probably dont even need the fucking debug.traceback spoof off you go boy")
-    return
-end
-
 local h; h = hookfunction(getrenv().debug.traceback, function(...)
     local res = h(...)
 
     if not checkcaller() and typeof(res) == "string" and string.match(res, exploitsrc) then
-        res = string.gsub(res, exploitsrc .. ":%d+\n", "")
-        res = string.gsub(res, exploitsrc .. ":%d+", "")
+        if exploitsrc ~= "" then
+            res = string.gsub(res, exploitsrc .. ":%d+\n", "")
+            res = string.gsub(res, exploitsrc .. ":%d+", "")
+        else
+            res = string.gsub(res, exploitsrc .. "\n:%d+\n", "\n")
+            res = string.gsub(res, exploitsrc .. "\n:%d+", "\n")
+        end
     end
 
     return res
