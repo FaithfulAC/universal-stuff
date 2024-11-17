e468e1aec72f86689c70bbbf1a3819e6aecd5226
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2024-11-17 10:19:54 -0800

    Update debug-traceback-bypass.lua

--- debug-traceback-bypass.lua
+++ debug-traceback-bypass.lua
@@ -1,12 +1,12 @@
 local exploitsrc = debug.info(1, "s") -- if your exploit src changes every execution you're fucked
-local split, match, gsub = string.split, string.match, string.gsub
+local split, match, gsub, remove = string.split, string.match, string.gsub, table.remove
 
 local h; h = hookfunction(getrenv().debug.traceback, function(...)
     local res = h(...)
 
     if not checkcaller() and typeof(res) == "string" and string.match(res, exploitsrc) then
         res = split(res, "\n")
-        res[1] = nil -- remove this hookfunction func trace
+        remove(res, 1) -- remove this hookfunction func trace
         res = table.concat(res, "\n") .. "\n"
         if exploitsrc ~= "" then
             res = gsub(res, exploitsrc .. ":%d+\n", "")
