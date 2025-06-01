2a494f0701be5394e751e2755ebd039251d16ab8
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2025-06-01 09:38:21 -0700

    Update true-secure-dex.lua
    condensed

--- true-secure-dex.lua
+++ true-secure-dex.lua
@@ -21,7 +21,7 @@ local getgenv, gethui, getrenv, hookmetamethod, hookfunction, identifyexecutor =
 	getgenv, gethui, getrenv, hookmetamethod, hookfunction, identifyexecutor;
 
 --[[
--- will not automatically run the script in an actor
+-- will automatically run the script in an actor
 if (not in_actor) and run_on_actor then
 	return run_on_actor(Instance.new("Actor"), "local in_actor = true;\n\n" .. game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/universal-stuff/main/true-secure-dex.lua"))
 end
@@ -78,10 +78,11 @@ end
 -- prevent solara from making the damn script error
 getgenv().cloneref = ((not identifyexecutor():lower():find("sol")) and cloneref) or function(...) return ... end
 
-repeat task.wait() until readfile(path) -- macsploit silly macsploit
+repeat task.wait() until isfile(path) and readfile(path) -- macsploit silly macsploit
 Api, gets = unpack(loadstring(readfile(path))())
 
 local DexAsset = "rbxassetid://17769765246"
+
  -- for GetAssetFetchStatus bypass
 if typeof(getgenv().AssetList) == "table" then
 	table.insert(getgenv().AssetList, DexAsset)
@@ -89,7 +90,7 @@ else
 	getgenv().AssetList = {DexAsset}
 end
 
-local RobloxGui = game:GetService("CoreGui").RobloxGui -- yeah good luck weaktable checking a restricted instance BOZO!
+local RobloxGui = game:GetService("CoreGui").RobloxGui
 local parent;
 if not RobloxGui:FindFirstChild("DexHost") then
 	parent = Instance.new("Folder", RobloxGui)
@@ -98,12 +99,12 @@ else
 	parent = RobloxGui:FindFirstChild("DexHost")
 end
 
--- prevent solara from making the damn script error (again)
-if LoadBypasses and (getrenv and hookmetamethod and hookfunction and not identifyexecutor():lower():find("solara")) then
+-- universal bypasses
+if LoadBypasses and (getrenv and hookmetamethod and hookfunction) then
 	task.spawn(loadstring(game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/universal-stuff/main/true-secure-dex-bypasses.lua")), nil, parent, in_actor)
 end
 
-task.wait(.05)
+task.wait(.2) -- give bypasses time to extract normal data and spoof around those ranges
 
 getgenv().Dex = game:GetObjects(DexAsset)[1]
 Dex.Parent = parent
@@ -204,16 +205,11 @@ local list = {
 for i, Script in pairs(scriptlist) do
 	-- set the scripts found in github to files in TSDex folder, so the script runs faster next time
 	local data;
-	if notupdated then
+	if notupdated or (not isfile(foldername .. list[i])) then
 		data = game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/TSD-script-storage/main" .. list[i])
 		writefile(foldername .. list[i], data)
 	else
 		data = readfile(foldername .. list[i])
-
-		if not data then
-			data = game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/TSD-script-storage/main" .. list[i])
-			writefile(foldername .. list[i], data)
-		end
 	end
 
 	local func = loadstring(data, "=" .. Script.Name)
